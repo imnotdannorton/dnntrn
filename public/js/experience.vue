7a6ab2c4c1e0157bab1b32d2ae5337daea0984fc
imnotdannorton <dnorton@dailybreak.com>
2017-12-05 22:50:11 -0500

    experience and about add

---
+++ public/js/experience.vue
@@ -0,0 +1,103 @@
+
+  <template>
+    <div class="welcome">
+      <!-- <h1 class="vibrant">Experience</h1> -->
+      <div align="center">
+        <h2 class="darkvibrant_bg_solid">CV</h2>
+      </div>
+      <div  v-for="item in sortedProjects" class="position">
+        <h2>{{item.fields.organization}} <em>{{item.fields.position}}</em><small>{{item.fields.dates}}</small></h2>
+        <small>{{item.fields.location}}</small>
+        <p class="darkmuted" v-html="markdown(item.fields.description)"></p>
+        <p class="darkmuted achievements-list" v-bind:style="{'border-color':activeColor()}" v-html="markdown(item.fields.achievementsList)"></p>
+      </div>
+      <hr class="muted muted_bg_solid"/>
+      
+      <div class="position">
+        
+        <h2>Education</h2>
+        <h3>Emerson College - <em>B.A. Visual and Media Arts</em><small class="dates">Sep, 2004 - Dec, 2007</small></h3>
+        <small>Boston, MA</small>
+        <p class="darkmuted achievements-list" v-bind:style="{'border-color':activeColor()}">
+          <ul>
+            <li>Focus in New Media</li>
+            <li>Minor in Photography</li>
+            <li>Graduated in 3.5 years</li>
+          </ul>
+        </p>
+      </div>
+      <hr class="muted muted_bg_solid"/>
+      <div class="position technical">
+        <h2>Technical Proficiencies</h2>
+        <p class="darkmuted achievements-list" v-bind:style="{'border-color':activeColor()}">
+          <ul>
+            <li class="header"><strong>Development Languages</strong></li>
+            <li>JavaScript (JQuery, NodeJS, AngularJS, Express, VueJS, CoffeeScript)</li>
+            <li>SASS/CSS</li>
+            <li>HTML</li>
+            <li>JADE</li>
+            <li>ActionScript3</li>
+            <li>PHP</li>
+          </ul>
+          <ul class="right">
+            <li class="header"><strong>Technologies</strong></li>
+            <li>AWS</li>
+            <li>Heroku</li>
+            <li>Grunt</li>
+            <li>NPM</li>
+            <li>SOAP/REST Web Services</li>
+            <li class="header"><strong>Software</strong></li>
+            <li>Photoshop</li>
+            <li>Illustrator</li>
+            <li>After Effects</li>
+          </ul>
+        </p>
+      </div>
+      <hr class="muted muted_bg_solid"/>
+      <div align="center">
+        <h2 class="darkvibrant_bg_solid download" v-on:click="">DOWNLOAD</h2>
+      </div>
+    </div>
+  </template>
+
+  <script>
+    import axios from 'axios';
+    import marked from 'marked';
+    export default {
+      name: 'experience',
+      data: () => ({
+        positions: [],
+        shades:["Vibrant","Muted","DarkVibrant","DarkMuted","LightVibrant","LightMuted"],
+        markdown: function(string){
+          return marked(string);
+        }
+      }),
+      computed:{
+        sortedProjects: function(){
+          return this.positions.sort(function(a, b){
+            return a.fields.sortIndex - b.fields.sortIndex
+          });
+        }
+      },
+      methods:{
+        activeColor:function(index){
+          var index = Math.floor(Math.random()*6);
+          var shade = window.palette[this.shades[index]];
+          var rgb = shade.rgb[0]+','+shade.rgb[1]+','+shade.rgb[2];
+          return 'rgb('+rgb+')';
+        } 
+      },
+      beforeMount() {
+        axios.all([
+          axios.get('/api/entries/position')
+        ]).then(([{ data: posData }]) => {
+          console.log(posData);
+          this.positions = posData.items;
+          
+        });
+      }
+      // components:{
+      //   Project
+      // },
+    };
+  </script>
